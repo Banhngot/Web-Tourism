3ae9ea0ea54d2a4e06bf75b7201585a101057203
Kachirou <hoang286272@gmail.com>
2023-07-07 11:46:05 +0700

    update

--- Front-end/tourism/src/Components/Home/Home.jsx
+++ Front-end/tourism/src/Components/Home/Home.jsx
@@ -1,32 +1,30 @@
-import React, {useEffect} from 'react'
-import './home.scss'
-import video from '../../Assets/video1.mp4'
-import {GrLocation} from 'react-icons/gr'
-import {HiFilter} from 'react-icons/hi'
-import {FiFacebook} from 'react-icons/fi'
-import {BsInstagram} from 'react-icons/bs'
-import {FaTripadvisor} from 'react-icons/fa'
-import {BsListTask} from 'react-icons/bs'
-import {TbApps} from 'react-icons/tb'
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import React, { useEffect } from "react";
+import "./home.scss";
+import video from "../../Assets/video1.mp4";
+import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { BsInstagram } from "react-icons/bs";
+import { FaTripadvisor } from "react-icons/fa";
+import { BsListTask } from "react-icons/bs";
+import { TbApps } from "react-icons/tb";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
   // hook to add a scroll animation...
-  useEffect(() =>{
-    Aos.init({duration: 2000})
-  }, [])
-
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
 
   return (
-    <section className='home'>
+    <section className="home">
       <div className="overlay"></div>
 
       <video src={video} muted autoPlay loop type="video1/mp4"></video>
 
       <div className="homeContent container">
         <div className="textDiv">
-
           <span data-aos="fade-up" className="smallText">
             hello
           </span>
@@ -34,58 +32,74 @@ const Home = () => {
           <h1 data-aos="fade-up" className="homeTitle">
             Search your Holiday
           </h1>
-
         </div>
 
         <div data-aos="fade-up" className="cardDiv grid">
-
-        <div className="destinationInput">
+          <div className="destinationInput">
             <label htmlFor="city">Search your destination:</label>
             <div className="input flex">
-              <input type="text" placeholder='Enter name here...'/>
-              <GrLocation className="icon"/>
+              <input type="text" placeholder="Enter name here..." />
+              <GrLocation className="icon" />
             </div>
-        </div>
 
-        <div className="dateInput">
+            <label htmlFor="city">Search your Hotel:</label>
+            <div className="input flex">
+              <input type="text" placeholder="Enter name here..." />
+              <GrLocation className="icon" />
+            </div>
+          </div>
+
+          <div className="dateInput">
             <label htmlFor="date">Select your day:</label>
             <div className="input flex">
-              <input type="date" placeholder='Enter name here...'/>
+              <input type="date" placeholder="Enter name here..." />
             </div>
-        </div>
 
-        <div className="priceInput">
+            <label htmlFor="date">Select your day:</label>
+            <div className="input flex">
+              <input type="date" placeholder="Enter name here..." />
+            </div>
+          </div>
+
+          <div className="priceInput">
             <div className="label_total flex">
               <label htmlFor="price">Max price:</label>
-              <h3 className='total'> $5000</h3>
+              <h3 className="total"> $5000</h3>
             </div>
             <div className="input flex">
               <input type="range" max="5000" min="1000" />
             </div>
-        </div>
 
-        <div className="searchOptions flex">
-          <HiFilter className="icon"/>
-          <span>MORE FILTERS</span>
-        </div>
+            <div className="label_total flex">
+              <label htmlFor="price">Max price:</label>
+              <h3 className="total"> $5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="1000" />
+            </div>
+          </div>
 
+          <div className="searchOptions flex">
+            <HiFilter className="icon" />
+            <button className="btn">SEARCH</button>
+          </div>
         </div>
 
         <div data-aos="fade-up" className="homeFooterIcons flex">
           <div className="rightIcons">
-            <FiFacebook className="icon"/>
-            <BsInstagram className="icon"/>
-            <FaTripadvisor className="icon"/>
+            <FiFacebook className="icon" />
+            <BsInstagram className="icon" />
+            <FaTripadvisor className="icon" />
           </div>
 
           <div className="leftIcons">
-          <BsListTask className="icon"/>
-          <TbApps className="icon"/>
+            <BsListTask className="icon" />
+            <TbApps className="icon" />
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

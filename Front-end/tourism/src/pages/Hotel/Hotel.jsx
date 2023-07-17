3b8c2c367c3e0c217b6ae04d44572363eb14a07b
Kachirou <hoang286272@gmail.com>
2023-07-17 12:00:29 +0700

    update

--- Front-end/tourism/src/pages/Hotel/Hotel.jsx
+++ Front-end/tourism/src/pages/Hotel/Hotel.jsx
@@ -1,7 +1,18 @@
 import "./hotel.css";
+import { getPlaceData } from "../../api";
+import { useEffect, useState } from "react";
 import Home from "../../Components/Home/Home";
 
 const Hotel = () => {
+  const [data, setData] = useState([]);
+  const [place, setPlace] = useState("");
+
+  useEffect(() => {
+    getPlaceData().then((data) => {
+      console.log(data);
+      setPlace(data);
+    });
+  }, []);
   //cap nhat
   return <Home />;
 };
